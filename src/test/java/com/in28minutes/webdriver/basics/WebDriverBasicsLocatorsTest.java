2df7034b2ccc9a9036bc5e7c0037438404e07b39
shinichi-ito <shinichi-ito@lotsjoys.com>
2020-04-30 20:47:49 +0900

    Locators Elements

--- src/test/java/com/in28minutes/webdriver/basics/WebDriverBasicsLocatorsTest.java
+++ src/test/java/com/in28minutes/webdriver/basics/WebDriverBasicsLocatorsTest.java
@@ -1,7 +1,9 @@
 package com.in28minutes.webdriver.basics;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -27,6 +29,34 @@ public class WebDriverBasicsLocatorsTest {
         assertEquals("First Web Application", driver.getTitle());
 
     }
+
+    @Test
+    public void testGetInformationAboutName() {
+        driver.get("http://localhost:8080/login");
+        WebElement nameElement = driver.findElement(By.id("name"));
+        System.out.println(nameElement.getTagName());//input
+        System.out.println(nameElement.getAttribute("type"));//text
+        System.out.println(nameElement.getAttribute("value"));//空
+    }
+
+    @Test
+    public void testGetInformationAboutPassword() {
+        driver.get("http://localhost:8080/login");
+        WebElement nameElement = driver.findElement(By.id("password"));
+        System.out.println(nameElement.getTagName());//input
+        System.out.println(nameElement.getAttribute("type"));//password
+        System.out.println(nameElement.getAttribute("value"));//空
+    }
+
+    @Test
+    public void testGetInformationAboutSubmitButton() {
+        driver.get("http://localhost:8080/login");
+        WebElement nameElement = driver.findElement(By.id("submit"));
+        System.out.println(nameElement.getTagName());//input
+        System.out.println(nameElement.getAttribute("type"));//submit
+        System.out.println(nameElement.getAttribute("value"));//空
+    }
+
     @AfterTest
     public void aftertTest() {
         driver.quit();
